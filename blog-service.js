7f73cf9c3d36df12a82f51ff975e95e75c8e26bf
usama-s786 <97623263+usama-s786@users.noreply.github.com>
2022-06-14 14:37:07 -0400

    Final Build

--- blog-service.js
+++ blog-service.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs") 
 
 var posts = []
 var categories = []
@@ -10,77 +10,143 @@ module.exports.initialize = () => {
         try {
 
             fs.readFile('./data/posts.json', (err, data) => {
-                if (err) throw err;
+                if (err) throw err 
 
-                posts = JSON.parse(data);
-                console.log("INITIALIZE - Posts.");
+                posts = JSON.parse(data) 
+                console.log("INITIALIZE - Posts.") 
             })
 
             fs.readFile('./data/categories.json', (err, data) => {
-                if (err) throw err;
+                if (err) throw err 
 
-                categories = JSON.parse(data);
-                console.log("INITIALIZE - Categories.");
+                categories = JSON.parse(data) 
+                console.log("INITIALIZE - Categories.") 
             })
 
         } catch (ex) {
-                      console.log("INITIALIZE - FAILURE.");
-                      reject("UNABLE TO READ THE FILE.");
+                      console.log("INITIALIZE - FAILURE.") 
+                      reject("UNABLE TO READ THE FILE.") 
                      }
-        console.log("INITIALIZE - SUCCESS.");
+        console.log("INITIALIZE - SUCCESS.") 
 
-        resolve("INITIALIZE - SUCCESS.");
+        resolve("INITIALIZE - SUCCESS.") 
     })
 
-    return promise;
-};
+    return promise 
+} 
 
 module.exports.getAllPosts = () => {
 
     var promise = new Promise((resolve, reject) => {
         
        if(posts.length === 0) {
-        var err = "no results returned";
-        reject({message: err});
+        var err = "no results returned" 
+        reject({message: err}) 
        }  
 
-        resolve(posts);
+        resolve(posts) 
     })
-    return promise;
-};
+    return promise 
+} 
 
 module.exports.getPublishedPosts = () => {
 
-    var pPosts = [];
+    var pPosts = [] 
     var promise = new Promise((resolve, reject) => {
       
-       for (var i=0; i < posts.length; i++){
+       for (var i=0;  i < posts.length;  i++){
            if (posts[i].published == true) {
-            pPosts.push(posts[i]);
+            pPosts.push(posts[i]) 
            }
        }
 
        if(pPosts.length === 0) {
-        var err = "no results returned";
-        reject({message: err});
+        var err = "no results returned" 
+        reject({message: err}) 
        }  
 
-    resolve (pPosts);
+    resolve (pPosts) 
     })
-    return promise;
-};
+    return promise 
+} 
 
 module.exports.getCategories = () => {
 
     var promise = new Promise((resolve, reject) => {
         if(categories.length === 0) {
-         var err = "no results returned";
-         reject({message: err});
+         var err = "no results returned" 
+         reject({message: err}) 
         }  
  
-     resolve (categories);
+     resolve (categories) 
      })
-     return promise;
-};
+     return promise 
+} 
 
+module.exports.addPost = (postData) => {
+   
+    var promise = new Promise((resolve, reject) => {
+        postData.published = (postData.published) ? true : false
+        postData.id= posts.length+1
+        posts.push(postData)
+ 
+        if(posts.length == 0) {
+         var err = "no results returned" 
+         reject({message: err}) 
+        }  
+        resolve (postData) 
+     })
 
+    return promise 
+}
+
+module.exports.getPostByCategory = (category) => {
+    var arrByCategory = [];
+    var promise = new Promise((resolve, reject) => {
+        
+        for(let i = 0; i<posts.length; i++){
+            if(posts[i].category == category) arrByCategory.push(posts[i])
+        }
+       if(arrByCategory.length === 0) {
+        var err = "no results returned" 
+        reject({message: err}) 
+       }  
+
+        resolve(arrByCategory) 
+    })
+    return promise 
+} 
+
+module.exports.getPostByMinDate = (minDateStr) => {
+    var arrByminDate = [];
+    var promise = new Promise((resolve, reject) => {
+        
+        for(let i = 0; i<posts.length; i++){
+            if(new Date (posts[i].postDate) >= new Date (minDateStr)) arrByminDate.push(posts[i])
+        }
+        if(arrByminDate.length === 0) {
+            var err = "no results returned" 
+            reject({message: err}) 
+        }  
+
+        resolve(arrByminDate) 
+    })
+    return promise 
+} 
+
+module.exports.getPostById = (id) => {
+    var arrById = [];
+    var promise = new Promise((resolve, reject) => {
+        
+        for(let i = 0; i<posts.length; i++){
+            if ( posts[i].id == id) arrById.push(posts[i])
+        }
+        if(arrById.length === 0) {
+            var err = "no results returned" 
+            reject({message: err}) 
+        }  
+
+        resolve(arrById) 
+    })
+    return promise 
+} 
--- server.js
+++ server.js
@@ -11,67 +11,165 @@
 *
 ********************************************************************************/ 
 
-var express = require("express");
+var express = require("express")
 var app = express()
 var blogService = require('./blog-service')
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 8080
+
+cloudinary.config({
+ cloud_name: 'usamasidat',
+ api_key: '854176769438177',
+ api_secret: 'ajqoPgr6G-GnlFCykF5znnkSsVk',
+ secure: true
+})
+
+const upload = multer() // no { storage: storage }
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
-  console.log("Express http server listening on port: " + HTTP_PORT);
+  console.log("Express http server listening on port: " + HTTP_PORT)
 }
 
-app.use(express.static('public')); 
+app.use(express.static('public'))
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
+app.get("/", (req,res)=>{
     res.redirect('/about')
 });
 
 // setup another route to listen on /about
-app.get("/about", function(req,res){
-    res.sendFile('views/about.html' , { root : __dirname});
+app.get("/about", (req,res)=>{
+    res.sendFile('views/about.html' , { root : __dirname})
 });
 
-app.get("/blog", function(req, res){
+app.get("/blog", (req, res)=>{
     blogService.getPublishedPosts()
         .then((data) => {
-        console.log ("getPublishedPosts displayed.");
-        res.json(data);
+        console.log ("getPublishedPosts displayed.")
+        res.json(data)
         })
         .catch((err) => {
-        console.log('ERROR MESSAGE:', err.message);
-        res.json(err);
+        console.log('ERROR MESSAGE:', err.message)
+        res.json(err)
         })
 });
 
-app.get("/posts", function(req, res){
-    blogService.getAllPosts()
-        .then((data) => {
-        console.log ("getAllPosts displayed.");
-        res.json(data);
+app.get("/posts", (req, res)=>{
+    if(req.query.category){
+        blogService.getPostByCategory(req.query.category)
+        .then((data)=>{
+            console.log ("getAllPosts by category displayed.")
+            res.json(data)
         })
-        .catch((err) => {
-        console.log('ERROR MESSAGE:', err.message);
-        res.json(err);
+        .catch((err)=>{
+            console.log('ERROR MESSAGE:', err.message)
+            res.json(err)
         })
+    }
+    else if(req.query.minDate){
+        blogService.getPostByMinDate(req.query.minDate)
+        .then((data)=>{
+            console.log ("getAllPosts by minDate displayed.")
+            res.json(data)
+        })
+        .catch((err)=>{
+            console.log('ERROR MESSAGE:', err.message)
+            res.json(err)
+        })
+    }
+    else{
+        blogService.getAllPosts()
+            .then((data) => {
+            console.log ("getAllPosts displayed.")
+            res.json(data)
+            })
+            .catch((err) => {
+            console.log('ERROR MESSAGE:', err.message)
+            res.json(err)
+        })
+    }
 });
 
-app.get("/categories", function(req, res){
+app.get("/post/:id", (req,res)=>{   
+    blogService.getPostById(req.params.id)
+            .then((data) => {
+            console.log ("getPostById displayed.")
+            res.json(data)
+            })
+            .catch((err) => {
+            console.log('ERROR MESSAGE:', err.message)
+            res.json(err)
+        })
+})
+
+app.get("/categories", (req, res)=>{
     blogService.getCategories()
         .then((data) => {
-        console.log ("getCategories displayed.");
-        res.json(data);
+        console.log ("getCategories displayed.")
+        res.json(data)
         })
         .catch((err) => {
-        console.log('ERROR MESSAGE:', err.message);
-        res.json(err);
+        console.log('ERROR MESSAGE:', err.message)
+        res.json(err)
         })
 });
 
+app.get("/posts/add", (req,res)=>{
+    res.sendFile('views/addPost.html' , { root : __dirname})
+})
+
+app.post("/posts/add", upload.single("featureImage"), (req,res)=>{
+let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+let stream = cloudinary.uploader.upload_stream((error, result) => {
+if (result) {
+resolve(result)
+}
+else {
+reject(error)
+}
+})
+
+streamifier.createReadStream(req.file.buffer).pipe(stream)
+})
+}
+
+async function upload(req) {
+let result = await streamUpload(req)
+console.log(result)
+return result
+}
+
+upload(req).then((uploaded)=> {
+req.body.featureImage = uploaded.url;
+//TODO: Process the req.body and add it as a new Blog Post before redirecting to/posts
+blogService.addPost(req.body)
+    .then((data)=> {
+    res.redirect('/posts')
+    })
+    .catch((err)=> { 
+    console.log('ERROR MESSAGE:', err.message)
+    })
+});
+})
+// })
+//   blogService.addPost(req.body)
+//   .then(()=>{
+//         console.log(req.body)
+//         res.redirect('/posts');
+//     })
+//     .catch((err)=>{
+//         res.send(err);
+//     })
+// })
+// })
+
 app.use((req,res)=>{
-    res.status(404).sendFile('/error.html' , { root : __dirname})
+    res.status(404).sendFile('/views/error.html' , { root : __dirname})
 })
 
 console.log ("Ready for initialize");
